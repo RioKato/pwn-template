c62f8eacc8e91df6b4a1b475f6adc32d7f23716c
Rio Kato <rio@kato.com>
2022-06-14 23:17:09 +0900

    updated

---
+++ script/dl-glibc.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+URL=ftp://ftp.gnu.org/gnu/glibc/
+
+list(){
+  curl -s "$URL" | awk '/glibc-[0-9]+\.[0-9]+(\.[0-9]+)?\.tar\.gz$/{print $9}'
+}
+
+download(){
+  curl -O "$URL""$1"
+}
+
+help(){
+  echo "$0 [-l | -d libc]"
+}
+
+
+while getopts ld: OPT
+do
+  case $OPT in
+    l) list
+      exit 0
+      ;;
+    d) download $OPTARG
+      exit 0
+      ;;
+  esac
+done
+
+help
